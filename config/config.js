71c6f8688891310b437d6e98f63b1f02e952eb89
Quang <quangfalu@gmail.com>
2020-08-20 02:16:29 +0700

    update

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const config = {
   env: process.env.NODE_ENV || 'development',
-  port: process.env.PORT || 3010,
+  port: process.env.PORT || 3001,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
   mongoUri: process.env.MONGODB_URI ||
     process.env.MONGO_HOST ||
